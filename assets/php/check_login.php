3933c2eaa34969471e38bc198466bdc5a53b5887
Tyler Wittig <tylerwittig@utexas.edu>
2020-11-25 00:39:42 -0600

    PHP Profile Popup and Login/Logout
    Created php folder. Removed profile popup copy/paste from all pages and pulled this data into new profile_popup.php file in php folder. Added include statement for this file to all pages in place of removed copy/paste code.
    
    Created check_login.php file in php folder. File called by profile_popup.php on login. Checks username and password with database and handles login.
    
    Added logout functionality to profile_popup.php.
    
    Changed avatar-tyler image from png to jpg.

---
+++ assets/php/check_login.php
@@ -0,0 +1,157 @@
+<?php
+
+
+/**
+* Display all errors.
+*/
+error_reporting(E_ALL);
+ini_set("display_errors", "on");
+
+
+/**
+* Function to unset username and password POST variables.
+* No return value.
+*/
+function doUnsetPost() {
+  unset($_POST['username']);
+  unset($_POST['password']);
+}
+
+
+/**
+* Function to check if a username
+* is in the iq_passwords database.
+* Returns 1 if username exists and 0 otherwise.
+*/
+function checkUser($mysqli, $username) {
+  $command = 'SELECT COUNT(1) FROM iq_passwords WHERE username = "' . $username . '";';
+  $result = $mysqli->query($command);
+  if (!$result) { die("Query failed: ($mysqli->error <br>"); }
+  // $user_in_db will be 1 if in database and 0 if not in database
+  $user_in_db = $result->fetch_row()[0];
+  return $user_in_db;
+}
+
+
+/**
+* Function to check if a username/password entry
+* is in the iq_passwords database.
+* Returns 1 if username/password entry exists and 0 otherwise.
+*/
+function checkPass($mysqli, $username, $password) {
+  $command = 'SELECT COUNT(1) FROM iq_passwords WHERE username = "' . $username . '" AND password = "' . $password . '";';
+  $result = $mysqli->query($command);
+  if (!$result) { die("Query failed: ($mysqli->error <br>"); }
+  // $pass_in_db will be 1 if in database and 0 if not in database
+  $pass_in_db = $result->fetch_row()[0];
+  return $pass_in_db;
+}
+
+
+/**
+* Function to insert a new username/password entry
+* into the iq_passwords database.
+* No return value.
+*/
+function doInsert($mysqli, $username, $password) {
+  $command = 'INSERT INTO iq_passwords VALUES ("' . $username . '", "' . $password . '");';
+  $result = $mysqli->query($command);
+  if (!$result) { die("Query failed: ($mysqli->error <br>"); }
+}
+
+
+/**
+* Function to build a return string to be displayed
+* based off of the given username and password.
+* String indicates successful login, incorrect password, or account created.
+* Returns this string.
+*/
+function buildResultString($mysqli, $username, $password) {
+  // Build String
+  $display_string = '';
+
+  if (!$username == '' && !$password == '') {
+    // username and password have values
+    $user_in_db = checkUser($mysqli, $username);
+
+    if ($user_in_db == 1) {
+      // username in database
+      $pass_in_db = checkPass($mysqli, $username, $password);
+
+      if ($pass_in_db == 1) {
+        // login successful
+        $display_string .= '<script>alert("Login Successful.");</script>';
+        setcookie('username', $username, time()+15, '/');
+
+      } else {
+        // password incorrect
+        $display_string .= '<script>alert("Incorrect Password.");</script>';
+      }
+
+    } else {
+      // username not in database
+      doInsert($mysqli, $username, $password);
+      $display_string .= '<script>alert("Account Created.");</script>';
+      setcookie('username', $username, time()+15, '/');
+    }
+  }
+
+  return $display_string;
+}
+
+
+/**
+* Engine function to configure required inputs for the above functions.
+* Checks database connection, pulls and handles username and password inputs,
+* and echoes return string
+*/
+function checkLogin() {
+
+  $server = "fall-2020.cs.utexas.edu";
+  $user   = "cs329e_bulko_wittig";
+  $pwd    = "format6arch4swamp";
+  $dbName = "cs329e_bulko_wittig";
+
+  // For debugging only
+  //echo "Server: ".$server."<br>";
+  //echo "User: ".$user."<br>";
+  //echo "Database name: ".$dbName."<br>";
+
+  // Connect to MySQL Server
+  $mysqli = new mysqli ($server, $user, $pwd, $dbName);
+
+  if ($mysqli->connect_errno) {
+    die('Connect Error: ' . $mysqli->connect_errno . ": " . $mysqli->connect_error);
+  } else {
+    echo '<script>console.log("connection established");</script>';
+  }
+
+  // Select Database
+  $mysqli->select_db($dbName) or die($mysqli->error);
+
+  // Retrieve data from POST
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  doUnsetPost();
+
+  // Escape User Input to help prevent SQL Injection
+  $username = $mysqli->real_escape_string($username);
+  $password = $mysqli->real_escape_string($password);
+
+  // Build Return String
+  $display_string = buildResultString($mysqli, $username, $password);
+  echo $display_string;
+
+  // Refresh Page and Die
+  header('Location: '.$_SERVER['PHP_SELF']);
+  die;
+}
+
+
+/**
+* Initial function call to execute script.
+*/
+checkLogin();
+
+
+?>
---
+++ assets/php/profile_popup.php
@@ -0,0 +1,121 @@
+<?php
+
+
+/**
+* Function to build and display the html contents of the profile popup
+* based on whether or not the user is logged in.
+* No return value.
+*/
+function buildPopup() {
+
+  // opening html
+  echo <<<TOP
+  <div class="popup-bg">
+    <article id="profile">
+      <input class="btn x r-float" type="button" onclick="closeForm()" value="X">
+TOP;
+  // user logged in popup html contents
+  if (isset($_COOKIE['username'])) {
+    $username = $_COOKIE['username'];
+    echo <<<USER
+      <h1>Good to see you, $username!</h1>
+      <form method="POST">
+        <p class="buttons">
+          <input class="btn" type="submit" value="Logout">
+        </p>
+      </form>
+USER;
+  // user not logged in popup html contents
+  } else {
+    echo <<<GUEST
+      <h1>Sign Up / Login</h1>
+      <p>
+        Create an account to start your quest!
+      </p>
+      <section>
+        <form id="login" method="POST">
+            <p>
+              Username:
+              <input name="username" type="text" size="10" placeholder="Enter Text Here" required />
+            </p>
+            <p>
+              Password:
+              <input name="password" type="text" size="10" placeholder="Enter Text Here" required />
+            </p>
+
+          <p class="buttons">
+            <input class="btn" id="submit" type="submit" value="Login">
+            <input class="btn" type="reset" value="Clear">
+          </p>
+        </form>
+      </section>
+GUEST;
+  }
+  // closing html
+  echo <<<BOTTOM
+    </article>
+  </div>
+BOTTOM;
+}
+
+
+/**
+* Function to log user out of website by unsetting cookie.
+* No return value.
+*/
+function logOut() {
+  setcookie('username', '', time()-3600, '/');
+  unset($_COOKIE['username']);
+  // Refresh Page and Die
+  header('Location: '.$_SERVER['PHP_SELF']);
+  die;
+}
+
+
+/**
+* Function to check and handle if username and password post values are set.
+* Calls check_login.php if so.
+*/
+function checkPostVariables() {
+
+  if (isset($_POST['username']) && isset($_POST['password'])) {
+    // post variables are set
+
+    if (($_POST['username'] != '') && ($_POST['password'] != '')) {
+      // post variables are nonempty
+
+      // check login - handle cases of homepage path vs not homepage path 
+      $url_path = $_SERVER['REQUEST_URI'];
+      $pieces = explode('/', $url_path);
+      $piece = $pieces[4];
+
+      if ($piece == '') {
+        // homepage
+        include './assets/php/check_login.php';
+      } else {
+        // journey or contact page ($piece == journey || contact)
+        include '../assets/php/check_login.php';
+      }
+      
+    } else {
+      // post variables are empty. something went wrong. unset them.
+      unset($_POST['username']);
+      unset($_POST['password']);
+    }
+  }
+
+  if ((isset($_COOKIE['username'])) && ('POST' === $_SERVER['REQUEST_METHOD'])) {
+    // user is logged in and clicked logout button
+    logOut();
+  }
+}
+
+
+
+/**
+* Initial Call to Build Popup
+*/
+buildPopup();
+checkPostVariables();
+
+?>
