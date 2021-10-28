897b8ac3ce1adfa61b5f71497b25df42446d72dd
Tyler <44188028+twit96@users.noreply.github.com>
2021-10-28 15:27:29 -0500

    Update header_journey_link.php
    tweak structure of html attributes to be included in link

--- assets/php/header_journey_link.php
+++ assets/php/header_journey_link.php
@@ -1,27 +1,29 @@
 <?php
 
 
-// Change behavior of header journey link based on whether user is logged in
+/**
+* Change behavior of header journey link based on
+* whether or not user is logged in
+*/
 if (isset($_COOKIE['username'])) {
   // Logged In
-
+  $journey_onclick = '';
   // handle cases of homepage path vs not homepage path
   $url_path = $_SERVER['REQUEST_URI'];
   $pieces = explode('/', $url_path);
   $piece = $pieces[2];
   // homepage
-  if ($piece == '') { $journey_href = 'journey/'; }
-  // contact page ($piece == journey || contact)
-  else { $journey_href = '../journey/'; }
-  $journey_onclick = '';
+  if ($piece == '') { $journey_href = ' href="journey/"'; }
+  // journey or contact page ($piece == journey || contact)
+  else { $journey_href = ' href="../journey/"'; }
 
 } else {
   // Logged Out
-  $journey_href = 'javascript:;';
-  $journey_onclick = 'toggleForm()';
+  $journey_onclick = ' onclick="toggleForm()"';
+  $journey_href = ' href="javascript:;"';
 }
 
-echo '<li><a id="journey-link" onclick="'.$journey_onclick.'" href="'.$journey_href.'">The Journey</a></li>';
+echo '<li><a id="journey-link"'.$journey_onclick.$journey_href.'>The Journey</a></li>';
 
 
 ?>
