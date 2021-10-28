6ab09fc79d95dc5e98760accfcd122327a8d64f2
Tyler <44188028+twit96@users.noreply.github.com>
2021-10-28 14:28:54 -0500

    Update header_journey_link.php
    fixed link pathname homepage vs not homepage cases

--- assets/php/header_journey_link.php
+++ assets/php/header_journey_link.php
@@ -1,13 +1,27 @@
 <?php
 
+
 // Change behavior of header journey link based on whether user is logged in
 if (isset($_COOKIE['username'])) {
-  $journey_href = 'journey/';
+  // Logged In
+
+  // handle cases of homepage path vs not homepage path
+  $url_path = $_SERVER['REQUEST_URI'];
+  $pieces = explode('/', $url_path);
+  $piece = $pieces[4];
+  // homepage
+  if ($piece == '') { $journey_href = 'journey/'; }
+  // contact page ($piece == journey || contact)
+  else { $journey_href = '../journey/'; }
   $journey_onclick = '';
+
 } else {
+  // Logged Out
   $journey_href = 'javascript:;';
   $journey_onclick = 'toggleForm()';
 }
+
 echo '<li><a id="journey-link" onclick="'.$journey_onclick.'" href="'.$journey_href.'">The Journey</a></li>';
+
 
 ?>
